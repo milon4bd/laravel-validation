133c0bfa5161c46329c6ff08b3dfb5491937db6f
Shimul Hossain <sbshimul000@gmail.com>
2021-08-31 20:19:44 +0600

    another change

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,6 +14,6 @@ class PostController extends Controller
     }
      
     public function store() {
-        return 'hello World My name is Shimul ';
+        return 'hello World My name is Shimul Hossain ';
     }
 }
